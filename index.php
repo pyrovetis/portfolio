071b3e10506f8bad634e657f2dcf6d186068482a
Sobhan <46265207+pyrovetis@users.noreply.github.com>
2025-02-09 02:43:47 +0330

    update email

--- index.php
+++ index.php
@@ -108,7 +108,7 @@
                             d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z"
                     />
                 </svg>
-                <a class="contrast" target="_blank" href="mailto:im@azarmanesh.dev"
+                <a class="contrast" target="_blank" href="mailto:sobhanking91@gmail.com"
                 >Business Email</a
                 >
             </li>
